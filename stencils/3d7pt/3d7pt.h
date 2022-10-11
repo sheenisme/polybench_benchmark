59b6d823738c55db85cc55704aa5fc53fde755aa
sheenisme <sheensong@163.com>
2022-10-11 19:35:15 +0800

    将3d7pt等用例的头文件脚本完善一下，同时更改deriche和nussionv的默认类型，将其均换为double

--- stencils/3d7pt/3d7pt.h
+++ stencils/3d7pt/3d7pt.h
@@ -1,78 +1,84 @@
-#ifndef _3D_7PT_H
-#define _3D_7PT_H
+#ifndef _3D7PT_H
+# define _3D7PT_H
 
-/* Default to 2，don't modify*/
-#define M 2
 
-/* Default to EXTRALARGE_DATASET. */
-#if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#define MEDIUM_DATASET
-#endif
 
-#if !defined(TSTEPS) && !defined(N)
+/* Default to MEDIUM_DATASET. */
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define MEDIUM_DATASET
+# endif
+
+# if !defined(TSTEPS) && !defined(M) && !defined(N)
 /* Define sample dataset sizes. */
-#ifdef MINI_DATASET
-#define TSTEPS 20
-#define N 10
-#endif
+#  ifdef MINI_DATASET
+#   define TSTEPS 20
+#   define M 2
+#   define N 40
+#  endif 
 
-#ifdef SMALL_DATASET
-#define TSTEPS 40
-#define N 20
-#endif
+#  ifdef SMALL_DATASET
+#   define TSTEPS 40
+#   define M 2
+#   define N 120
+#  endif 
 
-#ifdef MEDIUM_DATASET
-#define TSTEPS 240
-#define N 100
-#endif
+#  ifdef MEDIUM_DATASET
+#   define TSTEPS 100
+#   define M 2
+#   define N 400
+#  endif 
 
-#ifdef LARGE_DATASET
-#define TSTEPS 500
-#define N 120
-#endif
+#  ifdef LARGE_DATASET
+#   define TSTEPS 500
+#   define M 2
+#   define N 2000
+#  endif 
 
-#ifdef EXTRALARGE_DATASET
-// #define TSTEPS 1000
-// #define N 200
-#define TSTEPS 2048
-#define N 256
-#endif
+#  ifdef EXTRALARGE_DATASET
+#   define TSTEPS 1000
+#   define M 2
+#   define N 4000
+#  endif 
 
-#endif /* !(TSTEPS N) */
 
-#define _PB_TSTEPS POLYBENCH_LOOP_BOUND(TSTEPS, tsteps)
-#define _PB_N POLYBENCH_LOOP_BOUND(N, n)
+#endif /* !(TSTEPS M N) */
+
+# define _PB_TSTEPS POLYBENCH_LOOP_BOUND(TSTEPS,tsteps)
+# define _PB_M POLYBENCH_LOOP_BOUND(M,m)
+# define _PB_N POLYBENCH_LOOP_BOUND(N,n)
+
 
 /* Default data type */
-#if !defined(DATA_TYPE_IS_INT) && !defined(DATA_TYPE_IS_FLOAT) && !defined(DATA_TYPE_IS_DOUBLE)
-#define DATA_TYPE_IS_DOUBLE
-#endif
+# if !defined(DATA_TYPE_IS_INT) && !defined(DATA_TYPE_IS_FLOAT) && !defined(DATA_TYPE_IS_DOUBLE)
+#  define DATA_TYPE_IS_DOUBLE
+# endif
 
 #ifdef DATA_TYPE_IS_INT
-#define DATA_TYPE int
-#define DATA_PRINTF_MODIFIER "%d "
+#  define DATA_TYPE int
+#  define DATA_PRINTF_MODIFIER "%d "
 #endif
 
 #if defined(DATA_TYPE_IS_INT) && !defined(INTEGER_SUPPORT)
-#error "Integer data type not supported for this benchmark."
+#  error "Integer data type not supported for this benchmark."
 #endif
 
 #ifdef DATA_TYPE_IS_FLOAT
-#define DATA_TYPE float
-#define DATA_PRINTF_MODIFIER "%0.10f "
-#define SCALAR_VAL(x) x##f
-#define SQRT_FUN(x) sqrtf(x)
-#define EXP_FUN(x) expf(x)
-#define POW_FUN(x, y) powf(x, y)
-#endif
+#  define DATA_TYPE float
+#  define DATA_PRINTF_MODIFIER "%0.2f "
+#  define SCALAR_VAL(x) x##f
+#  define SQRT_FUN(x) sqrtf(x)
+#  define EXP_FUN(x) expf(x)
+#  define POW_FUN(x,y) powf(x,y)
+# endif
 
 #ifdef DATA_TYPE_IS_DOUBLE
-#define DATA_TYPE double
-#define DATA_PRINTF_MODIFIER "%0.10lf "
-#define SCALAR_VAL(x) x
-#define SQRT_FUN(x) sqrt(x)
-#define EXP_FUN(x) exp(x)
-#define POW_FUN(x, y) pow(x, y)
-#endif
+#  define DATA_TYPE double
+#  define DATA_PRINTF_MODIFIER "%0.2lf "
+#  define SCALAR_VAL(x) x
+#  define SQRT_FUN(x) sqrt(x)
+#  define EXP_FUN(x) exp(x)
+#  define POW_FUN(x,y) pow(x,y)
+# endif
 
-#endif /* _3D_7PT_H */
+#endif /* !_3D7PT_H */
+
--- utilities/polybench.spec
+++ utilities/polybench.spec
@@ -20,12 +20,17 @@ gramschmidt	linear-algebra/solvers	double	FALSE	M N	20 30	60 80	200 240	1000 120
 lu	linear-algebra/solvers	double	FALSE	N	40	120	400	2000	4000
 ludcmp	linear-algebra/solvers	double	FALSE	N	40	120	400	2000	4000
 trisolv	linear-algebra/solvers	double	FALSE	N	40	120	400	2000	4000
-deriche	medley	float	FALSE	W H	64 64	192 128	720 480	4096 2160	7680 4320
+deriche	medley	double	FALSE	W H	64 64	192 128	720 480	4096 2160	7680 4320
 floyd-warshall	medley	double	TRUE	N	60	180	500	2800	5600
-nussinov	medley	int	TRUE	N	60	180	500	2500	5500
+nussinov	medley	double	TRUE	N	60	180	500	2500	5500
 adi	stencils	double	FALSE	TSTEPS N	20 20	40 60	100 200	500 1000	1000 2000
+fdtd-1d	stencils	double	FALSE	TSTEPS N	20 30	40 80 	100 40	500 2000	800000 50000
 fdtd-2d	stencils	double	FALSE	TMAX NX NY	20 20 30	40 60 80	100 200 240	500 1000 1200	1000 2000 2600
+heat-1d	stencils	double	FALSE	TSTEPS M N	20 2 100	40 2 200	100 2 400	500 2 2000	200000 2 50000
+heat-2d	stencils	double	FALSE	TSTEPS M N	20 2 10	40 2 20	100 2 40	500 2 120	8000 2 3000
 heat-3d	stencils	double	FALSE	TSTEPS N	20 10	40 20	100 40	500 120	1000 200
 jacobi-1d	stencils	double	FALSE	TSTEPS N	20 30	40 120	100 400	500 2000	1000 4000
 jacobi-2d	stencils	double	FALSE	TSTEPS N	20 30	40 90	100 250	500 1300	1000 2800
 seidel-2d	stencils	double	FALSE	TSTEPS N	20 40	40 120	100 400	500 2000	1000 4000
+3d7pt	stencils	double	FALSE	TSTEPS M N	20 2 40	40 2 120	100 2 400	500 2 2000	1000 2 4000
+3d27pt	stencils	double	FALSE	TSTEPS M N	20 2 40	40 1 120	100 2 400	500 2 2000	1000 2 4000
