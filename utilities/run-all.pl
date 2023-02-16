66149ef83d264279f85f5c5c1684607e03db7815
sheenisme <sheensong@163.com>
2023-02-16 19:49:06 +0800

    完善测试脚本,以便同时用run_all.pl进行不同类型的测试.同时修改了一个中文字符的bug in get_performance_test_results.sh

--- utilities/run-all.pl
+++ utilities/run-all.pl
@@ -9,7 +9,8 @@
 my $TARGET_DIR = ".";
 
 if ($#ARGV != 0 && $#ARGV != 1) {
-   printf("usage perl run-all.pl target-dir [output-file]\n");
+   printf("usage perl run-all.pl target-dir [option] [output-file]\n");
+   printf("the option: 1.Reliable performance test(default);2.Performance distribution test;3.Performance and Error test;4.LNLAMP test.\n");
    exit(1);
 }
 
@@ -19,9 +20,14 @@ if ($#ARGV >= 0) {
    $TARGET_DIR = $ARGV[0];
 }
 
+my $OPTION = 1;
+if ($#ARGV >= 1) {
+   $OPTION = $ARGV[1];
+}
+
 my $OUTFILE = "";
-if ($#ARGV == 1) {
-   $OUTFILE = $ARGV[1];
+if ($#ARGV == 2) {
+   $OUTFILE = $ARGV[2];
 }
 
 
@@ -41,10 +47,21 @@ foreach $cat (@categories) {
         next if (!(-d $target.'/'.$dir));
 
         my $kernel = $dir;
-        my $targetDir = $target.'/'.$dir;
-        # my $command = "cd ../scripts; ./performance_testing.sh $targetDir $kernel; ./get_performance_test_results.sh $kernel";
-        my $command = "cd ../scripts; ./experimental_tests.sh  $targetDir $kernel; ./get_experimental_test_result.sh $kernel";
-        # my $command = "cd $targetDir; lnlamp $kernel.c";
+        my $targetDir = $target.'/'.$dir;  
+        my $command = "";
+        if ($OPTION == 1) {
+            $command = "cd ../scripts; ./Reliable_perf_test.sh $targetDir $kernel;";
+        }elsif($OPTION == 2) {
+            $command = "cd ../scripts; ./performance_testing.sh $targetDir $kernel; ./get_performance_test_results.sh $kernel";
+        }elsif($OPTION == 3) {
+            $command = "cd ../scripts; ./experimental_tests.sh  $targetDir $kernel; ./get_experimental_test_result.sh $kernel";
+        }elsif($OPTION == 4) {
+            $command = "cd $targetDir; lnlamp $kernel.c";
+        }else {
+            # $command = "cd $targetDir; make clean; make; ./$kernel";
+            printf("the error option!!!\n");
+            exit(1);
+        }              
            $command .= " 2>> $OUTFILE" if ($OUTFILE ne '');
         print($command."\n");
         system($command);
