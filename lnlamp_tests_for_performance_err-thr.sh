02c4aca4a91319d2c010aade6cd99637427880d6
sheenisme <sheensong@163.com>
2023-03-25 10:41:50 +0800

    进行误差阈值下的性能测试.

---
+++ lnlamp_tests_for_performance_err-thr.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+workdir=$(cd `dirname $0`; pwd)
+# echo "当前的工作路径是:" $workdir
+cd $workdir
+
+err_thr="-e 1"
+
+
+all_benchs=$(cat ./utilities/benchmark_list_performance)
+for bench in $all_benchs;
+do
+    benchdir=$(dirname $bench)
+    benchname=$(basename $benchdir)
+    # echo $benchdir " " $benchname
+    # 进入测试用例的目录进行测试
+    cd $benchdir
+
+    # 根据测试用例，选择不同的调度算法
+    case "${benchname}" in
+        seidel-2d|jacobi-2d|fdtd-2d|trmm|doitgen|gemm|2mm|3mm)
+            lnlamp -a feautrier -t '{[1,4,8,16,32]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -a feautrier -t {[1,4,8,16,32]} ${benchname}.c ${err_thr} over! "
+            ;;
+        heat-3d)
+            lnlamp -a feautrier -t '{[1,1,8,16,16]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -a feautrier -t {[1,1,8,16,16]} ${benchname}.c ${err_thr} over! "
+            ;;
+        correlation)
+            lnlamp -t '{[8,8,8,8,8]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -t '{[8,8,8,8,8]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        covariance)
+            lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[16,16,8,8,8]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[16,16,8,8,8]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        gramschmidt)
+            lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -a feautrier -t '{[16,8,8,8,8]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -a feautrier -t {[16,8,8,8,8]} ${benchname}.c ${err_thr} over! "
+            ;;
+        lu)
+            lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[64,64,64,64]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[64,64,64,64]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        cholesky)
+            lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[16,16,16,16,16]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -i '--isl-schedule-max-coefficient=1 --isl-schedule-max-constant-term=0 ' -t '{[16,16,16,16,16]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        symm)
+            lnlamp -t '{[256,256,256,256,256]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -t '{[256,256,256,256,256]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        adi)
+            lnlamp -t '{[8,8,8,8,8]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -t '{[8,8,8,8,8]}' ${benchname}.c ${err_thr} over! "
+            ;;
+        *)
+            lnlamp -t '{[1,4,8,16,32]}' ${benchname}.c ${err_thr}
+            echo "lnlamp -t '{[1,4,8,16,32]}' ${benchname}.c ${err_thr} over! "
+    esac
+    echo ""
+
+    # 返回测试脚本目录
+    cd $workdir
+done
+
+# 返回测试脚本目录
+cd $workdir
+rm -rf build
+rm -rf result-out
+
+./taffo_collect-fe-stats.sh "temp_test_res_${err_thr}"
+
+echo "lnlamp temp test, over!"
+# 返回测试脚本目录
+cd $workdir
